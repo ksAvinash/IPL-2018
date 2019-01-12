141b4af46461f8cfaa5f792b78e6d18df79d4d83
ksAvinash <justmailtoavi@gmail.com>
2019-01-12 09:07:15 +0530

    5.04 update commit
    1. Suggest Questions feature to include Author name
    2. Removed the CricBuzz API calls
    3. Revised Ads placement

---
+++ app/src/main/gen/works/avijay/com/ipl2018/R.java
@@ -0,0 +1,7 @@
+/*___Generated_by_IDEA___*/
+
+package works.avijay.com.ipl2018;
+
+/* This stub is only used by the IDE. It is NOT the R class actually packed into the APK */
+public final class R {
+}
